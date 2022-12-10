ef6199fc28f9c8f861b55f318854dad51329e255
AnnaIvanenk <anchouze@mail.ru>
2022-12-10 18:02:33 +0300

    Программа готова

--- ИтогиБлока/Program.cs
+++ ИтогиБлока/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк 
+// формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+
+string[] family = new String[7] { "Я", "мама", "папа", "сын", "собака", "", "иЯ"}; ;
+
+for (int i = 0; i < family.Length; i++)
+{
+    Console.WriteLine(family[i]);
+}
+
+System.Console.WriteLine();
+System.Console.WriteLine();
+
+string[] newFamily = new String[7];
+
+for (int i = 0; i < family.Length; i++)
+{
+    if (family[i].Length <= 3)
+    {
+        newFamily [i] = family[i];
+        Console.WriteLine(newFamily[i]);
+        }
+}
+
+
+
+
 
